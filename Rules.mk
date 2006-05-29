45da5df3ee4b6442de3536f7da1506a41ee95d8d
Jonathan Coome <jcoome@users.berlios.de>
2006-05-29 18:42:04 +0000

    Make the dist Makefile target a bit more sensible.
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/scmpc/trunk@43 996bc42b-cd0d-0410-b191-afae628fd629

--- Rules.mk
+++ Rules.mk
@@ -28,15 +28,16 @@ maintainer-clean: distclean
 dist: depend clean
 	@echo Creating $(program_name_version).tar.bz2
 	@rm -f ./$(program_name_version).tar.bz2
-	@cp -r ../$(program_name) ../$(program_name_version)
-	@tar -cjvf ../$(program_name_version).tar.bz2 --wildcards \
+	@rm -f ./$(program_name_version)
+	@ln -s ../$(shell basename $${PWD}) $(program_name_version)
+	@tar -cjvf ./$(program_name_version).tar.bz2 --dereference --wildcards \
 		--exclude "autom4te.cache" --exclude "*.swp" --exclude "scmpc.conf" \
 		--exclude "config.log" --exclude "config.status" --exclude "Makefile" \
 		--exclude "config.h" --exclude "stamp-h1" --exclude "*.o" \
-		--exclude ".svn" \
-		../$(program_name_version)/
-	@mv ../$(program_name_version).tar.bz2 .
-	@rm -fr ../$(program_name_version)
+		--exclude ".svn" --exclude $(program_name_version).tar.bz2 \
+		--exclude $(program_name_version)/$(program_name_version) \
+		./$(program_name_version)/
+	@rm -f ./$(program_name_version)
 
 check:
 	@echo Currently there are no tests for this program...
