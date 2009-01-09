1b6aaf991d5e07c92066d5891a0a7ad21bd775c5
Christoph Mende <angelos@unkreativ.org>
2009-01-09 21:38:56 +0100

    Change license to GPL-2, same as scmpc-0.2

--- src/audioscrobbler.c
+++ src/audioscrobbler.c
@@ -1,10 +1,26 @@
 /**
  * audioscrobbler.c: Audioscrobbler backend.
  *
+ * ==================================================================
  * Copyright (c) 2008 Christoph Mende <angelos@unkreativ.org>
- * All rights reserved. Released under the 2-clause BSD license.
- *
  * Based on Jonathan Coome's work on scmpc
+ *
+ * This file is part of scmpc.
+ *
+ * scmpc is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * scmpc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with scmpc; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ==================================================================
  */
 
 
