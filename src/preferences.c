a4c3becccf033f6865489bdd2600ebc2a5d45ec8
Christoph Mende <angelos@unkreativ.org>
2009-01-09 18:51:09 +0100

    Added parse_files() and free_config_files()

--- src/preferences.c
+++ src/preferences.c
@@ -57,6 +57,57 @@ static int cf_validate_num_zero(cfg_t *cfg, cfg_opt_t *opt)
 	return 0;
 }
 
+static void free_config_files(char **config_files)
+{
+	short int i;
+	for(i=0;i<3;i++)
+		free(config_files[i]);
+}
+
+static int parse_files(cfg_t *cfg)
+{
+	short int i;
+	char *home, *config_files[3];
+
+	home = getenv("HOME");
+
+	if(home == NULL) {
+		config_files[0] = strdup("");
+		config_files[1] = strdup("");
+	} else {
+		if((asprintf(&(config_files[0]),"%s/.scmpcrc",home)) == -1)
+			exit(EXIT_FAILURE);
+		if((asprintf(&(config_files[1]),"%s/.scmpc/scmpc.conf",home)) == -1) {
+			free(config_files[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	config_files[2] = strdup(SYSCONFDIR "/scmpc.conf");
+
+	for(i=0;i<3;i++)
+	{
+		if(config_files[i] == NULL)
+			exit(EXIT_FAILURE);
+
+		switch(cfg_parse(cfg,config_files[i]))
+		{
+			case CFG_PARSE_ERROR:
+				fprintf(stderr,"%s: This configuration file "
+				"contains errors and cannot be parsed.\n",
+				config_files[i]);
+				free_config_files(config_files);
+				return 1;
+			case CFG_SUCCESS:
+				free_config_files(config_files);
+				return 0;
+			default:
+				return 1;
+		}
+	}
+	free_config_files(config_files);
+	return 0;
+}
+
 void init_preferences(int argc, char *argv[])
 {
 	cfg_t *cfg, *sec_as, *sec_mpd;
@@ -93,10 +144,14 @@ void init_preferences(int argc, char *argv[])
 	cfg_set_validate_func(cfg,"mpd|port",cf_validate_num);
 	cfg_set_validate_func(cfg,"mpd|timeout",cf_validate_num);
 
-	if(!parse_files(cfg)) {
+	if(parse_files(cfg) != 0) {
 		cfg_free(cfg);
 		return;
 	}
+
+	sec_as = NULL;
+	sec_mpd = NULL;
+	printf("%p%p\n",(void*)sec_as,(void*)sec_mpd);
 
 	free(prefs.log_file);
 	free(prefs.pid_file);
