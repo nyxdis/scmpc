328b019184e44d1516ccfefac2e530360fc4f201
Christoph Mende <angelos@unkreativ.org>
2009-01-11 22:00:47 +0100

    Implement server_connect_{unix,tcp}()

--- src/mpd.c
+++ src/mpd.c
@@ -26,10 +26,13 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/un.h>
+#include <netinet/in.h>
 
 #include "misc.h"
 #include "mpd.h"
@@ -38,12 +41,41 @@
 static int server_connect_unix(const char *path)
 {
 	int sockfd;
+	struct sockaddr_un addr;
+	unsigned int len;
+
+	sockfd = socket(AF_UNIX,SOCK_STREAM,0);
+	if(sockfd < 0)
+		return -1;
+
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path,path,strlen(path) + 1);
+	len = strlen(addr.sun_path) + sizeof(addr.sun_family);
+	if(connect(sockfd,(struct sockaddr *)&addr,len) < 0)
+		return -1;
+
 	return sockfd;
 }
 
-static int server_connect_tcp(const char *hostname, int port)
+static int server_connect_tcp(const char *host, int port)
 {
 	int sockfd;
+	struct sockaddr_in addr;
+	struct hostent *he;
+	unsigned int len;
+
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd < 0)
+		return -1;
+
+	he = gethostbyname(host);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	memcpy((char *)&addr.sin_addr.s_addr,(char *)he->h_addr,he->h_length);
+	len = sizeof(addr);
+	if(connect(sockfd,(struct sockaddr *)&addr,len) < 0)
+		return -1;
+
 	return sockfd;
 }
 
@@ -53,12 +85,12 @@ void mpd_connect(void)
 		mpd_sockfd = server_connect_unix(prefs.mpd_hostname);
 	else
 		mpd_sockfd = server_connect_tcp(prefs.mpd_hostname,prefs.mpd_port);
-	
+
 	if(mpd_sockfd < 0) {
 		scmpc_log(ERROR,"Failed to connect to MPD: %s",strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if(fcntl(mpd_sockfd,F_SETFL,fcntl(mpd_sockfd,F_GETFL,0) | O_NONBLOCK) < 0)
 		exit(EXIT_FAILURE);
 }
